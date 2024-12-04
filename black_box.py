385b4d757f075151862e472512fd084fe55841d5
saarinenemma <81643935+saarinenemma@users.noreply.github.com>
2024-12-04 08:39:42 -0500

    Update black_box.py

--- black_box.py
+++ black_box.py
@@ -4,29 +4,10 @@ from transport import transport_torch as transport_pure_gpu
 def black_box(DA, SB, C, delta, device, ymax):
 	rows = len(C) 
 	cols = len(C[0])
-	"""C_comp = [[0 for i in range(cols)] for j in range(rows)]
-	for i in range(rows):
-		for j in range(cols): 
-			if C[i][j]>=ymax or C[i][j]==0:
-				C_comp[i][j] = ymax
-				print("bad")
-			else:
-				C_comp[i][j]=C[i][j]
-				print("good",i," ",j)"""
 
-	
-	#mask = C < ymax
 	C_temp=C.clone()
 	C_temp[C_temp>=ymax]=ymax
-	#mask2 = C_temp>= ymax
-	#size=C.size()
-	
-	#C_comp = C[mask]
 	print("test", C_temp)
-	#C_temp=C_temp[mask2]
-	#C_comp=C_comp.add(C_temp)
-	
-	#C_tensor = torch.tensor(C_comp, device=device, requires_grad=False)
 	print("success")
 	Mb, yA, yB, ot_pyt_loss, iteration = transport_pure_gpu(DA, SB, C_temp, delta, device=device)
 	for i in range(rows):
@@ -39,12 +20,16 @@ def RPW_approx(DA, SB, C, delta, p, device):
 	Fi_est=1
 	rows = len(C) 
 	cols = len(C[0])
-	C_scale = [[0 for i in range(cols)] for j in range(rows)]
+	'''C_scale = [[0 for i in range(cols)] for j in range(rows)]
 	for i in range(rows):
 		for j in range(cols): 
 			C_scale=C[i][j]**p
-			C_scale=C[i][j]*Fi_est
-	Mb, yA, yB, ot_pyt_loss, iteration=black_box(DA, SB, C_scale, delta, device, Fi_est)
+			C_scale=C[i][j]*Fi_est'''
+	C_scale=C.clone()
+	C_scale=C_scale.pow(p)
+	C_scale=C_scale*Fi_est
+	ymax=min(1,(Fi_est**p)/delta
+	Mb, yA, yB, ot_pyt_loss, iteration=black_box(DA, SB, C_scale, delta, device, ymax)
 	p_root=1/p
 	ot_pyt_loss=ot_pyt_loss**p_root
 	return Mb, yA, yB, ot_pyt_loss, iteration
