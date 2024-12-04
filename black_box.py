f6791bee25bdb4cea5e5613ff4964d1785b96664
saarinenemma <81643935+saarinenemma@users.noreply.github.com>
2024-12-04 09:13:57 -0500

    Update black_box.py

--- black_box.py
+++ black_box.py
@@ -17,19 +17,23 @@ def black_box(DA, SB, C, delta, device, ymax):
 				Mb[i][j]=0
 	return Mb, yA, yB, ot_pyt_loss, iteration
 def RPW_approx(DA, SB, C, delta, p, device):
-	Fi_est=1
-	rows = len(C) 
-	cols = len(C[0])
-	'''C_scale = [[0 for i in range(cols)] for j in range(rows)]
-	for i in range(rows):
-		for j in range(cols): 
-			C_scale=C[i][j]**p
-			C_scale=C[i][j]*Fi_est'''
-	C_scale=C.clone()
-	C_scale=C_scale.pow(p)
-	C_scale=C_scale*Fi_est
-	ymax=min(1,(Fi_est**p)/delta)
-	Mb, yA, yB, ot_pyt_loss, iteration=black_box(DA, SB, C_scale, delta, device=device, ymax=ymax)
-	p_root=1/p
-	ot_pyt_loss=ot_pyt_loss**p_root
-	return Mb, yA, yB, ot_pyt_loss, iteration
+	low=0
+	high=1
+	while low<=(high):
+		Fi_est=(high+low)//2
+	
+		C_scale=C.clone()
+		C_scale=C_scale.pow(p)
+		C_scale=C_scale*Fi_est
+		ymax=min(1,(Fi_est**p)/delta)
+		Mb, yA, yB, ot_pyt_loss, iteration=black_box(DA, SB, C_scale, delta, device=device, ymax=ymax)
+		p_root=1/p
+		ot_pyt_loss=ot_pyt_loss**p_root
+		untrans=Mb.sum()
+		if ot_pyt_loss>(1-untrans)+delta:
+			high=Fi_est
+		elif ot_pyt_loss<(1-untrans)-delta:
+			low=Fi_est
+		else:
+			return Mb, yA, yB, ot_pyt_loss, iteration
+	
