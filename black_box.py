dc9c0532436318ea6d84d435b5ec0b79fe62dc15
saarinenemma <81643935+saarinenemma@users.noreply.github.com>
2024-12-04 05:17:29 -0500

    Update black_box.py

--- black_box.py
+++ black_box.py
@@ -4,16 +4,15 @@ def black_box(DA, SB, C, delta, device, ymax):
 	rows = len(C) 
 	cols = len(C[0])
 	C_comp = [[0 for i in range(cols)] for j in range(rows)]
-	for i in range(rows): 
-    for j in range(cols): 
+	for i in range(rows):
+		for j in range(cols): 
 			if C[i][j]>=ymax or C[i][j]==0:
         C_comp[i][j] = ymax
 			else:
 				C_comp[i][j]=C[i][j]
-	Mb, yA, yB, ot_pyt_loss, iteration = transport_pure_gpu(DA_tensor, SB_tensor, cost_tensor, delta_tensor, device=device)
-	
-	for i in range(rows): 
-    for j in range(cols): 
+	Mb, yA, yB, ot_pyt_loss, iteration = transport_pure_gpu(DA, SB, C_comp, delta, device=device)
+	for i in range(rows):
+		for j in range(cols): 
 			if C[i][j]>=ymax :
 				ot_pyt_loss -= Mb[i][j]*ymax
 				Mb[i][j]=0
