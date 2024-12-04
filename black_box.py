08f40ea37801a5d2b3386a6d0257d0c5358fd628
saarinenemma <81643935+saarinenemma@users.noreply.github.com>
2024-12-04 05:40:47 -0500

    Update black_box.py

--- black_box.py
+++ black_box.py
@@ -7,7 +7,7 @@ def black_box(DA, SB, C, delta, device, ymax):
 	for i in range(rows):
 		for j in range(cols): 
 			if C[i][j]>=ymax or C[i][j]==0:
-        C_comp[i][j] = ymax
+				C_comp[i][j] = ymax
 			else:
 				C_comp[i][j]=C[i][j]
 	Mb, yA, yB, ot_pyt_loss, iteration = transport_pure_gpu(DA, SB, C_comp, delta, device=device)
@@ -17,5 +17,16 @@ def black_box(DA, SB, C, delta, device, ymax):
 				ot_pyt_loss -= Mb[i][j]*ymax
 				Mb[i][j]=0
 	return Mb, yA, yB, ot_pyt_loss, iteration
-def RPW_approx(DA, SB, C, delta, device):
-	
+def RPW_approx(DA, SB, C, delta, p, device):
+	Fi_est=1
+	rows = len(C) 
+	cols = len(C[0])
+	C_scale = [[0 for i in range(cols)] for j in range(rows)]
+	for i in range(rows):
+		for j in range(cols): 
+			C_scale=C[i][j]**p
+			C_scale=C[i][j]*Fi_est
+	Mb, yA, yB, ot_pyt_loss, iteration=black_box(DA, SB, C_scale, delta, device, Fi_est)
+	p_root=1/p
+	ot_pyt_loss=ot_pyt_loss**p_root
+	return Mb, yA, yB, ot_pyt_loss, iteration
